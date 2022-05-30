53c81037eba0e721f67e165d55fca56146d7e229
David Jeong <drumrobot@1do.space>
2022-05-30 13:02:57 +0900

    Turn on ES module interoperability

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,12 +1,12 @@
 import { ConfigService } from '@nestjs/config';
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     inject: [ConfigService],
     useFactory(configService: ConfigService): Promise<typeof mongoose> {
-      let uri = configService.get('MONGO_URI');
+      const uri = configService.get('MONGO_URI');
       return mongoose.connect(uri);
     },
   },
